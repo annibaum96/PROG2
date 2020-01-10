00d5fe85524edb9c6008ea8666cbc3054437f120
annibaum96 <47819007+annibaum96@users.noreply.github.com>
2020-01-10 15:17:10 +0100

    Termin Erfassen

--- plannerbrunch/main.py
+++ plannerbrunch/main.py
@@ -2,16 +2,27 @@ from flask import Flask, render_template, request
 
 app = Flask("Planner")
 
-@app.route('/brunch', methods = ['POST', 'GET'])
-def formular():
+@app.route('/index', methods = ['POST', 'GET'])
+def startseite():
 	if request.method == 'POST':
 		result=request.form['name']
-		return render_template('index.html',result = result)
+		return render_template('header.html',result = result)
 	else:
-		return render_template('index.html')
+		return render_template('header.html')
 
-@app.route('/')
-
+@app.route('/erfassen', methods = ['POST', 'GET'])
+def erfassen():
+	if request.method == 'POST':
+		akt=request.form['aktivitaet']
+		date=request.form['datum']
+		time=request.form['beginn']
+		time2=request.form['ende']
+		who=request.form['verantwortung']
+		who2=request.form.getlist('beteiligt')
+		print(akt, date, time, time2, who, who2)
+		return render_template('termin_erfassen.html', len=len, akt=akt, who2=who2)
+	else:
+		return render_template('termin_erfassen.html', len=len)
 
 if __name__ == "__main__":
-	app.run(debug=True, port=5000)
+    app.run(debug=True, port=5000)
