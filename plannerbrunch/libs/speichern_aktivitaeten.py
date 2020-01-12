5c4a2e7a912b6ba1cd56f7136f0a6dc7d1beef7e
annibaum96 <47819007+annibaum96@users.noreply.github.com>
2020-01-12 14:34:19 +0100

    trying json/dict

--- plannerbrunch/libs/speichern_aktivitaeten.py
+++ plannerbrunch/libs/speichern_aktivitaeten.py
@@ -2,32 +2,84 @@ import json
 
 # Struktur Dictionary
 
-def aktivitaet_speichern(name, datum, beginn, ende, verantwortung, beteiligung):
-	data = {}
-	data['Aktivitaet'] = []
-	data['Aktivitaet'].append({
+def aktivitaet_speichern(name, datum, beginn, ende, verantwortung, beteiligung):	
+	
+	jahresplan_daten = load_json()
+	aktivitaeten = jahresplan_daten.get("jahresplan", {})
+
+	aktivitaet = {
 	    'name': name,
 	    'datum': datum,
 	    'beginn': beginn,
 	    'ende': ende,
 	    'verantwortung': verantwortung,
 	    'beteiligung': beteiligung
-	})
+	}
 
-	with open('data.txt', 'w') as outfile:
-	    json.dump(data, outfile)
+	save_json(aktivitaeten)	
 
 def load_json():
-    json_daten = {}
+    jahresplan = {}
+    # jahresplan['aktivitaet'] = []
     try:
         with open('data/data.json') as open_file:
             json_daten = json.load(open_file)
     
     except FileNotFoundError:    
-        print("File not found")
+        json_daten = {
+        	'jahresplan': {
+        		'aktivitaet': {
+
+        		}
+        	}
+        }
 
     return json_daten
 
-def save_to_json(daten):
-    with open('data/data.json', "w", encoding="utf-8") as open_file:
-        json.dump(daten, open_file)
+def save_json():
+   with open('data/data.json', 'w', encoding = "utf-8") as open_file:
+	    json.dump(jahresplan, aktivitaeten, indent = 2)
+
+
+# import json
+
+# # Struktur Dictionary
+
+# def aktivitaet_speichern(name, datum, beginn, ende, verantwortung, beteiligung):	
+	
+# 	jahreplan_daten = load_json()
+# 	aktivitaeten = jahresplan_daten.get("aktivitaet", {})
+
+# 	jahresplan['aktivitaet'].append({
+# 	    'name': name,
+# 	    'datum': datum,
+# 	    'beginn': beginn,
+# 	    'ende': ende,
+# 	    'verantwortung': verantwortung,
+# 	    'beteiligung': beteiligung
+# 	})
+
+
+
+
+	
+
+# def load_json():
+#     jahresplan = {}
+#     # jahresplan['aktivitaet'] = []
+#     try:
+#         with open('data/data.json') as open_file:
+#             json_daten = json.load(open_file)
+    
+#     except FileNotFoundError:    
+#         json_daten = {
+#         	'aktivitaet': {
+
+#         	}
+#         }
+
+#     return json_daten
+
+# def save_to_json():
+#    with open('data/data.json', 'w', encoding = "utf-8") as jahresplan_data:
+# 	    json.dump(jahresplan, jahresplan_data, indent = 2)
--- plannerbrunch/main.py
+++ plannerbrunch/main.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from libs import speichern_aktivitaeten
 
 app = Flask("Planner")
+app.secret_key = 'kn/Ujlkm#[sdfnop]jnnq/km*'
 
+#Startseite
 @app.route('/index', methods = ['POST', 'GET'])
+@app.route('/')
 def startseite():
 	if request.method == 'POST':
 		result=request.form['name']
@@ -11,21 +14,32 @@ def startseite():
 	else:
 		return render_template('index.html')
 
+
+
+
+
+#Seite um eine Aktivität nach der anderen zu erfassen
 @app.route('/erfassen', methods = ['POST', 'GET'])
 def erfassen():
 	if request.method == 'POST':
+		flash('Die Aktivität wurde erfolgreich erfasst.')
 		akt=request.form['aktivitaet']
 		date=request.form['datum']
 		time=request.form['beginn']
 		time2=request.form['ende']
 		who=request.form['verantwortung']
 		who2=request.form.getlist('beteiligt')
-		print(akt, date, time, time2, who, who2)
-		returned_date = speichern_aktivitaeten.aktivitaet_speichern(akt, date, time, time2, who, who2)
+		input_data = speichern_aktivitaeten.aktivitaet_speichern(akt, date, time, time2, who, who2)
 		return render_template('termin_erfassen.html', len=len, akt=akt, who2=who2)
+
 	else:
 		return render_template('termin_erfassen.html', len=len)
 
+
+
+
+
+#Hier können alle erfassten Daten geprüft und gegebenenfalls gelöscht werden
 @app.route('/uebersicht', methods = ['POST', 'GET'])
 def overview():
 	if request.method == 'POST':
@@ -34,6 +48,11 @@ def overview():
 	else:
 		return render_template('overview_termine.html')
 
+
+
+
+
+#Seite um gesamte erfassten Daten in Kalender zu exportieren
 @app.route('/export', methods = ['POST', 'GET'])
 def export():
 	if request.method == 'POST':
